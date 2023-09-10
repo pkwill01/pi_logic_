e828d077c369d592d7dc9bf4a758126e5ed9784b
PatK <patrick.kingwill@roboticsystems.com.au>
2023-09-10 19:54:07 +1000

    Added super basic sin plots

--- inc/mainwindow.h
+++ inc/mainwindow.h
@@ -3,6 +3,17 @@
 
 #include <QMainWindow>
 
+// Qt Libaries 
+#include <QtCharts/QChart>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+// General Libaries 
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <thread>
+#include<unistd.h>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,5 +28,13 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QtCharts::QLineSeries *series;
+    QtCharts::QChart *chart;
+    QtCharts::QChartView *chartView;
+    float maxTime = 10;
+
+private slots:
+    void PlotPressed();
+    void TimePressed();
 };
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Connecting buttons to functions
+    connect(ui->plotButton, SIGNAL(released()), this, SLOT(PlotPressed()));
+    connect(ui->timeButton, SIGNAL(released()), this, SLOT(TimePressed()));
+
+
+    // Init the Plot series 
+    series = new QtCharts::QLineSeries();
+    chart = new QtCharts::QChart();
+    chartView = new QtCharts::QChartView(chart);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +23,47 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+void MainWindow::PlotPressed()
+{
+    // Plotting Pre checks
+    chart->removeSeries(series);
+    maxTime = 10;
+    // Creating Data series 
+    for (float i = 0; i < maxTime; i += .5) 
+    {
+     series->append(i, sin(i));
+    }
+
+
+    // Chart options
+    chart->addSeries(series);
+    chart->setTitle("Sin Waves");
+    chart->createDefaultAxes();
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom); 
+
+    // Adding Chart options
+    // Enable both zooming and panning
+    chartView->setRubberBand(QtCharts::QChartView::RectangleRubberBand);
+    chartView->setDragMode(QtCharts::QChartView::ScrollHandDrag);
+
+    // Adding the Plot to frame 
+    ui->plotFrame->layout()->addWidget(chartView);
+}
+void MainWindow::TimePressed()
+{
+    // Clearing old series 
+    series->clear();
+    
+    // adding new data to series 
+    maxTime += 20;
+    for (float i = 0; i < maxTime; i += .5) 
+    {
+     series->append(i, sin(i));
+    }
+
+    // Realign axis 
+    chart->axisX()->setRange(0, maxTime);
+}
